78d2c4ab392b42f11b2c5066a2ffdb70a898f6bf
Patryk W <>
2021-09-15 20:46:13 +0200

    Improved migration and customization of adding new categories

--- db/migrate/20210904234732_create_category_jobs.rb
+++ db/migrate/20210904234732_create_category_jobs.rb
@@ -1,6 +1,6 @@
 class CreateCategoryJobs < ActiveRecord::Migration[6.1]
   def change
-    create_table :category_job do |t|
+    create_table :category_jobs do |t|
       t.string :category_name
       t.string :category_description
 
